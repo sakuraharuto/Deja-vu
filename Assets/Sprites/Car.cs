bc94f82da17916208fe4761216fc1005fce5ec56
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2021-11-27 18:34:15 -0500

    Car movement

--- Assets/Sprites/Car.cs
+++ Assets/Sprites/Car.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+
+    [SerializeField] float steerSpeed = 200;
+    [SerializeField] float moveSpeed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
