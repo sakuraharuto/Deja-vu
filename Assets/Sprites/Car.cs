24393e872bb4f18b9b0fe465b1dffdc8a78a1e4d
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2021-11-28 23:51:23 -0500

    Crash Detection and reload

--- Assets/Sprites/Car.cs
+++ Assets/Sprites/Car.cs
@@ -7,19 +7,21 @@ public class Car : MonoBehaviour
 
     [SerializeField] float steerSpeed = 200;
     [SerializeField] float moveSpeed = 10;
+    bool canMove = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Rotate(0, 0, -steerAmount);
-        transform.Translate(0, moveAmount, 0);
+        if (canMove){
+            transform.Rotate(0, 0, -steerAmount);
+            transform.Translate(0, moveAmount, 0);
+        }
+    }
+
+    public void DisableControl(){
+        canMove= false;
     }
 }
---
+++ Assets/Sprites/CrashDetect.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetect : MonoBehaviour
+{
+    [SerializeField] float reloadDelay = 1f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Wall" && !hasCrashed){
+            Debug.Log("Bonk");
+            hasCrashed = true;
+
+            //Disable control
+            FindObjectOfType<Car>().DisableControl();
+
+            //Play Effect
+
+            //Play SFX
+
+            Invoke("ReloadScene", reloadDelay);
+        }
+    }
+
+    void ReloadScene(){
+        SceneManager.LoadScene(0);
+    }
+
+}
